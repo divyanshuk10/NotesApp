5afb7beab45c82fb797302be33690182574d2fbf
Divyanshu Kumar <divyanshu.kumar@agreeya.com>
2019-09-04 18:14:37 +0530

    injected Note view model into Main activity.

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.notesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Log.d("TAG", "onChanged: " + notes.toArray().toString());
+            }
+        });
     }
 }
