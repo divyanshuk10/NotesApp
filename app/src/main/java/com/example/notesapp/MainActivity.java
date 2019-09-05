c74c6871ce3f9fc06eb79f2b83bbbd81478bd372
Divyanshu Kumar <divyanshu.kumar@agreeya.com>
2019-09-05 10:15:27 +0530

    Main activity populated with dummy data.

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,28 +1,39 @@
 package com.example.notesapp;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private NoteViewModel noteViewModel;
+    private RecyclerView rv_notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rv_notes = findViewById(R.id.rv_notes);
+        rv_notes.setLayoutManager(new LinearLayoutManager(this));
+        rv_notes.setHasFixedSize(true);
+
+
+        final NoteAdapter adapter = new NoteAdapter();
+        rv_notes.setAdapter(adapter);
+
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                Log.d("TAG", "onChanged: " + notes.toArray().toString());
+                adapter.setNotes(notes);
             }
         });
     }
